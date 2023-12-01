2fa7d7402679c20a342db0fac748f8dff0a7faa8
Equinox <info@importantpeople.nl>
2023-12-01 13:09:44 +0100

    Added basic websocket client in client.py

--- Server/client.py
+++ Server/client.py
@@ -1,19 +1,90 @@
 import asyncio
+import json
+import time
 import websockets
+import threading
 
 
-class GameServerClient:
+class WebSocketClient:
+    def __init__(self, uri):
+        self.uri = uri
+        # Message Router
+        self.message_router = {
+            "players": self.handle_player_msg,
+            "highscores": self.handle_highscore_msg
+        }
+        self.loop = asyncio.new_event_loop()
+        self.thread = threading.Thread(target=self.start_loop, args=(self.loop,))
+        self.thread.start()
 
-    # WebSocket client function with queue as an argument
-    async def client(self, queue):
-        uri = "ws://localhost:8765"
-        async with websockets.connect(uri) as websocket:
-            while True:
-                data = await websocket.recv()
-                queue.put(data)  # Put data into the queue
+    ###########################
+    # Client Connection Methods
+    ###########################
 
-    # Function to run the client in a separate thread
-    def start_client(self, queue):
-        asyncio.new_event_loop().run_until_complete(self.client(queue))
+    def start_loop(self, loop):
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(self.connect())
+
+    async def connect(self):
+        async with websockets.connect(self.uri) as websocket:
+            await self.listen(websocket)
+
+    async def listen(self, websocket):
+        async for message in websocket:
+            print(f"Received message: {message}")
+
+    def send_message(self, message):
+        asyncio.run_coroutine_threadsafe(self._send(message), self.loop)
+
+    async def _send(self, message):
+        async with websockets.connect(self.uri) as websocket:
+            await websocket.send(message)
+
+    def close(self):
+        self.loop.call_soon_threadsafe(self.loop.stop)
+        self.thread.join()
+
+    ###########################
+    # Business Logic Methods
+    ###########################
+
+    def handle_player_msg(self, message):
+        raise NotImplementedError("This method (handle_player) is not yet implemented")
+
+    def handle_highscore_msg(self, message):
+        raise NotImplementedError("This method (handle_highscore) is not yet implemented")
 
 
+    ###########################
+    # Message Handling Methods
+    ###########################
+
+    # Function to handle incoming messages
+    async def handle_message(self, message):
+        data = json.loads(message)
+        action = data.get("action")
+        params = data.get("params", {})
+
+        if action in self.message_router:
+            response = self.message_router[action](**params)
+            return response
+        else:
+            return "Unknown action"
+
+
+# Example usage
+if __name__ == "__main__":
+    client = WebSocketClient("wss://socketsbay.com/wss/v2/1/demo/")
+
+    # Example sending a message
+    client.send_message(json.dumps({"action": "request_latest_players", "params": {"name": "This player name"}}))
+
+    # Simulate main program running
+    try:
+        while True:
+            print("Main loop doing stuff")
+            # Example sending a message
+            client.send_message(json.dumps({"action": "greet", "params": {"name": "Alice"}}))
+            time.sleep(1)
+    except KeyboardInterrupt:
+        client.close()
--- Server/server_game_data.py
+++ Server/server_game_data.py
@@ -1,18 +1,18 @@
 class ServerGameData:
     def __init__(self):
-        self.players: list[str] = []
+        self.latest_players: list[str] = []
         self.highscores: list[int] = []
         self.max_amount_of_players = 5
         self.max_amount_of_highscores = 10
 
-    def add_player(self, player: str):
-        if len(self.players) < self.max_amount_of_players:
-            self.players.append(player)
+    def ingest_player(self, player: str):
+        if len(self.latest_players) < self.max_amount_of_players:
+            self.latest_players.append(player)
         else:
-            self.players.pop(0)
-            self.players.append(player)
+            self.latest_players.pop(0)
+            self.latest_players.append(player)
 
-    def add_highscore(self, highscore: int):
+    def ingest_highscore(self, highscore: int):
         if len(self.highscores) < self.max_amount_of_highscores:
             self.highscores.append(highscore)
         else:
