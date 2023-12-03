fd4bd8617e8f958bc09302862fcd72218c5ca398
Equinox <info@importantpeople.nl>
2023-12-03 16:32:37 +0100

    Working server. Dear lord what did it take.

--- Server/base_client.py
+++ Server/base_client.py
@@ -4,7 +4,7 @@ import time
 import websockets
 import threading
 from Settings import settings
-from abc import ABC, abstractmethod
+from abc import ABC
 
 
 class BaseWebSocketClient(ABC):
@@ -14,28 +14,12 @@ class BaseWebSocketClient(ABC):
         self.port = port if port else settings.SERVER_PORT
         self.logging = settings.SERVER_DEBUG_MODE
 
-        # Message Router
-        self.message_router = self.setup_message_router()
-        if not self.message_router:
-            raise Exception("Message router not set up. Please add actions and their "
-                            "corresponding methods.")
-
         # Asyncio and Threading startup
+        self.websocket = None
+        self.running = True
         self.loop = asyncio.new_event_loop()
         self.thread = threading.Thread(target=self.start_loop, args=(self.loop,))
         self.thread.start()
-
-    ###########################
-    # Setup Methods
-    ###########################
-
-    @abstractmethod
-    def setup_message_router(self) -> dict[str, callable]:
-        """
-        Sets up the message router by adding actions and their corresponding methods.
-        :return:
-        """
-        pass
 
     ###########################
     # Client Connection Methods
@@ -47,23 +31,44 @@ class BaseWebSocketClient(ABC):
 
     async def connect(self):
         if self.uri:
-            async with websockets.connect(self.uri) as websocket:
-                await self.listen(websocket)
+            try:
+                if self.logging:
+                    print(f"Connecting to {self.uri}:{self.port}")
+                self.websocket = await websockets.connect(f"{self.uri}:{self.port}")
+                await self.listen(self.websocket)
+            except ConnectionRefusedError:
+                print(f"Could not connect to {self.uri}:{self.port}\n"
+                      "Reason: Connection refused")
+                self.close()
 
     async def listen(self, websocket):
         async for message in websocket:
             await self.handle_message(message)
 
     def send_message(self, message):
-        asyncio.run_coroutine_threadsafe(self._send(message), self.loop)
+        if self.logging:
+            print(f"Sending message: {message}")
+        if not self.websocket:
+            time.sleep(1)
+        if self.websocket and self.websocket.closed:
+            self.loop.run_until_complete(self.connect())
+        if self.websocket and not self.websocket.closed:
+            asyncio.run_coroutine_threadsafe(self._send(message), self.loop)
+        else:
+            print("WebSocket is closed. Cannot send message.")
 
     async def _send(self, message):
-        async with websockets.connect(f"{self.uri}:{self.port}") as websocket:
-            await websocket.send(message)
+        try:
+            await self.websocket.send(message)
+        except websockets.exceptions.ConnectionClosed:
+            print("WebSocket connection is closed.")
 
     def close(self):
+        self.running = False
+        if self.websocket:
+            asyncio.run_coroutine_threadsafe(self.websocket.close(), self.loop)
         self.loop.call_soon_threadsafe(self.loop.stop)
-        self.thread.join()
+        print("Client closed.")
 
     ###########################
     # Message Handling Methods
@@ -72,10 +77,10 @@ class BaseWebSocketClient(ABC):
     # Function to handle incoming messages
     async def handle_message(self, message):
         data = json.loads(message)
-        action = data.get("action")
-        params = data.get("params", {})
         if self.logging:
             print(f"Received message: {message}")
+        action = data.get("action")
+        params = data.get("params", {})
 
         if action in self.message_router:
             response = self.message_router[action](**params)
